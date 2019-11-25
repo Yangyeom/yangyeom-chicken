1c6f8d8e08acc2a55c021cc5e57ab7a812f318c9
양혜진 <yang94lol@naver.com>
2019-11-25 14:08:20 +0900

    HJ | Django | Review API

--- yangyeom-django/movies/serializers.py
+++ yangyeom-django/movies/serializers.py
@@ -6,13 +6,24 @@ class GenreSerializers(serializers.ModelSerializer):
         model = Genre
         fields = '__all__'
 
+class ReviewSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
 class MovieSerializers(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
 
 class GenreDetailSerializers(serializers.ModelSerializer):
-    movies= MovieSerializers(many=True)
+    movies = MovieSerializers(many=True)
     class Meta(GenreSerializers.Meta): 
         fields = GenreSerializers.Meta.fields
         include = ['movies']
+
+# class MovieDetailSerializers(serializers.ModelSerializer):
+#     reviews = ReviewSerializers(many=True)
+#     class Meta:
+#         fields = MovieSerializers.Meta.fields
+#         include = ['reviews']
