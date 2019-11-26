43df61a303b3dbe17b10e89261ca6a165e115eeb
양혜진 <yang94lol@naver.com>
2019-11-26 11:32:01 +0900

    HJ | Vue | Review

--- yangyeom-django/movies/views.py
+++ yangyeom-django/movies/views.py
@@ -65,16 +65,18 @@ def movie_reviews(request, movie_pk):
             review.movie.watched_users.add(review.user)
             # print('요청 보내짐')
             return Response(serializer.data)
-# ===================================================================================================
+
 @api_view(['PUT', 'DELETE'])
 def review_update_delete(request, movie_pk, review_pk):
     if request.method == 'PUT':
         pass
     else:
         print('요청받음')
+        print(request.data)
         review = Review.objects.get(pk=review_pk)
         review.delete()
         return Response('삭제되었습니다')
+# ===================================================================================================
 
 @login_required
 def review_create(request, movie_pk):
